4590f4e85181eaa3601c18437c70d3035691c518
Vedant224 <118610480+Vedant224@users.noreply.github.com>
2025-02-03 20:14:31 +0530

    Update Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -30,7 +30,7 @@ const Card = ({ title, description, imageUrl, projectLink }) => {
           rel="noopener noreferrer"
           className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-500 rounded-lg hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-blue-300"
         >
-          Read more
+          Visit Page
           <svg
             className="ml-2 -mr-1 w-4 h-4"
             fill="currentColor"

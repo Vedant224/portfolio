636d46f5103c013a935281cbfe7e45d7dfe61d33
Vedant224 <118610480+Vedant224@users.noreply.github.com>
2025-04-02 17:44:01 +0530

    Update Contact.jsx

--- src/views/Contact.jsx
+++ src/views/Contact.jsx
@@ -105,7 +105,7 @@ const Contact = () => {
               href="hello"
               className="mb-12 mt-4 font-semibold text-blue-700 block uppercase"
             >
-              +91 8285631499
+              +91 
             </a> */}
             <h1 className="text-3xl font-bold">Email</h1>
             <a
